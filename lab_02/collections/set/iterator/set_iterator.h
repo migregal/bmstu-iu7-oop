fa17749cf4ba436d4a9ff1bcce8e109fb77657f8
migregal <devmigregal@gmail.com>
2021-04-11 19:31:26 +0300

    Iterator operators updated

--- lab_02/collections/set/iterator/set_iterator.h
+++ lab_02/collections/set/iterator/set_iterator.h
@@ -14,11 +14,15 @@ class set_iterator : public std::iterator<std::input_iterator_tag, T> {
     std::weak_ptr<set_node<T>> cur;
 
 public:
+    set_iterator();
+    explicit set_iterator(const std::shared_ptr<set_node<T>> &node);
+    set_iterator(const set_iterator<T> &iterator);
     ~set_iterator() = default;
 
+    virtual void next();
+
+    set_node<T> *operator->();
     set_node<T> &operator*();
-
-
     set_iterator<T> &operator++();
 
     bool operator!=(const set_iterator<T> &iterator) const;
--- lab_02/collections/set/iterator/set_iterator.hpp
+++ lab_02/collections/set/iterator/set_iterator.hpp
@@ -5,6 +5,33 @@
 #ifndef LAB_02_SET_ITERATOR_HPP
 #define LAB_02_SET_ITERATOR_HPP
 
+#include "set_iterator.h"
+
+template<typename T>
+set_iterator<T>::set_iterator() {
+    this->cur.lock() = nullptr;
+}
+
+template<typename T>
+set_iterator<T>::set_iterator(const std::shared_ptr<set_node<T>> &node) {
+    this->cur = node;
+}
+
+template<typename T>
+set_iterator<T>::set_iterator(const set_iterator<T> &iterator) {
+    this->cur = iterator.cur.lock();
+}
+
+template<typename T>
+void set_iterator<T>::next() {
+    this->cur = this->cur.lock()->get_next();
+}
+
+template<typename T>
+set_node<T> *set_iterator<T>::operator->() {
+    return this->cur.lock().get();
+}
+
 template<typename T>
 set_node<T> &set_iterator<T>::operator*() {
     return *this->cur.lock();
@@ -12,17 +39,18 @@ set_node<T> &set_iterator<T>::operator*() {
 
 template<typename T>
 set_iterator<T> &set_iterator<T>::operator++() {
+    this->next();
     return *this;
 }
 
 template<typename T>
 bool set_iterator<T>::operator!=(const set_iterator<T> &iterator) const {
-    return false;
+    return this->cur.lock() != iterator.cur.lock();
 }
 
 template<typename T>
 bool set_iterator<T>::operator==(const set_iterator<T> &iterator) const {
-    return false;
+    return this->cur.lock() == iterator.cur.lock();
 }
 
 #endif//LAB_02_SET_ITERATOR_HPP
