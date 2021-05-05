473014d421f5ec2173fd9d09a585be902574f539
migregal <devmigregal@gmail.com>
2021-05-06 00:10:14 +0300

    Model details added

---
+++ lab_03/libs/driver/src/objects/details.cpp
@@ -0,0 +1,25 @@
+#include <objects/details.h>
+
+void ModelDetails::add_point(const Point &pt) {
+    this->points.push_back(pt);
+}
+
+void ModelDetails::add_edge(const Edge &link) {
+    this->edges.push_back(link);
+}
+
+const Vector<Point> &ModelDetails::get_points() const {
+    return this->points;
+}
+
+const Vector<Edge> &ModelDetails::get_edges() const {
+    return this->edges;
+}
+
+void ModelDetails::reform(const Point &_move, const Point &_scale, const Point &_turn) {
+    for (auto &pt : points) {
+        pt.move(_move.get_x(), _move.get_y(), _move.get_z());
+        pt.scale(_scale.get_x(), _scale.get_y(), _scale.get_z());
+        pt.rotate(_turn.get_x(), _turn.get_y(), _turn.get_z());
+    }
+}
---
+++ lab_03/libs/driver/src/objects/details.h
@@ -0,0 +1,22 @@
+#include <vector/vector.h>
+
+#include <objects/edge.h>
+#include <objects/point.h>
+
+class ModelDetails {
+public:
+    ModelDetails() = default;
+    ModelDetails(Vector<Point> &_points, Vector<Edge> &_edges) : points(_points), edges(_edges){};
+    ~ModelDetails() = default;
+
+    void add_point(const Point &pt);
+    void add_edge(const Edge &edge);
+    [[nodiscard]] const Vector<Point> &get_points() const;
+    [[nodiscard]] const Vector<Edge> &get_edges() const;
+
+    void reform(const Point &move, const Point &scale, const Point &rotate);
+
+private:
+    Vector<Point> points;
+    Vector<Edge> edges;
+};
