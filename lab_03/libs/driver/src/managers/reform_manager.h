48761b72a25439ff2a8e3b61450e38368ca0e418
migregal <devmigregal@gmail.com>
2021-05-06 12:02:33 +0300

    Reform manager added

---
+++ lab_03/libs/driver/src/managers/reform_manager.h
@@ -0,0 +1,19 @@
+#ifndef REFORM_MANAGER_H
+#define REFORM_MANAGER_H
+
+#include <managers/base_manager.h>
+#include <object.h>
+
+class ReformManager : public BaseManager {
+public:
+    ReformManager() = default;
+    ~ReformManager() = default;
+
+    static void reform_object(const std::shared_ptr<Object> &_object, const Point &move, const Point &scale, const Point &turn);
+
+    static void move_object(const std::shared_ptr<Object> &_object, const double &dx, const double &dy, const double &dz);
+    static void scale_object(const std::shared_ptr<Object> &_object, const double &kx, const double &ky, const double &kz);
+    static void turn_object(const std::shared_ptr<Object> &_object, const double &ox, const double &oy, const double &oz);
+};
+
+#endif
